9df085dac47b0ed1ba453dbb759ad1f4ebed071f
LeonAli5366 <leonali274784@gmail.com>
2024-10-13 02:52:46 +0600

    test

--- index.js
+++ index.js
@@ -3,14 +3,11 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const cors = require("cors");
 const app = express();
 
 app.use(bodyParser.json());
-app.all("/", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
+app.use(cors());
 
 const dbURI =
   "mongodb+srv://LeonAli:t0dqsPodJVqk61DK@cluster0.pmffn8w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
